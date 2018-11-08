741895852443144d0f6d84838a346c6ba907c001
Eduardo Reye <reduardo.dev21@gmail.com>
2018-11-07 22:10:06 -0600

    Inicio de componentes

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('contenido.contenido');
 });
 
-Auth::routes();
+//Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
