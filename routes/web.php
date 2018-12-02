31cdd9aa46e647a124df517bdd54cdc2afa5163c
Carlos Morán <carlosmoran.97cr@gmail.com>
2018-12-02 14:04:04 -0600

    cambios por errores de naun

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('contenido/contenido');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Rutas de las especialidades
+Route::get('/especialidad', 'EspecialidadController@index');
+Route::post('/especialidad/registrar', 'EspecialidadController@store');
+Route::put('/especialidad/actualizar', 'EspecialidadController@update');
+Route::get('/especialidad/selectEspecialidad', 'EspecialidadController@selectEspecialidad');
+
+//Rutas de los signos vitales
+Route::get('/signosvitales', 'SignoVitalController@index');
+Route::post('/signosvitales/registrar', 'SignoVitalController@store');
+Route::put('/signosvitales/actualizar', 'SignoVitalController@update');
+
+//Rutas de Medicamentos
+Route::get('/medicamento', 'MedicamentoController@index');
+Route::post('/medicamento/registrar', 'MedicamentoController@store');
+Route::put('/medicamento/actualizar', 'MedicamentoController@update');
+
+//Rutas para Servicio
+Route::get('/servicio', 'ServicioController@index');
+Route::post('/servicio/registrar', 'ServicioController@store');
+Route::put('/servicio/actualizar', 'ServicioController@update');
+
+//Rutas para Profesionales en Medicina
+Route::get('/pem', 'ProfesionalEnMedicinaController@index');
+Route::post('/pem/registrar','ProfesionalEnMedicinaController@store');
+Route::put('/pem/actualizar', 'ProfesionalEnMedicinaController@update');
+
+//Rutas de trabajador
+Route::get('/trabajador/selectTrabajador','TrabajadorController@selectTrabajador');
